185c34b9fb398107ff44a3a8ec3fe0a80e33b988
Ian Germann <ianjgermann@gmail.com>
2022-12-04 14:03:19 -0500

    Golfed day3 part 1

--- day3/run.js
+++ day3/run.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 
-const lines = fs.readFileSync('input.txt', 'utf8').toString().split('\n')
+const i = fs.readFileSync('input.txt', 'utf8').toString()
+const lines = i.split('\n')
 
 // a-z -> 97 - 122 -> 1-26
 // A-Z -> 65 - 90 ->  27-52
@@ -16,13 +17,16 @@ const part1 = () =>
     .map(getPriority)
     .reduce((sum, priority) => sum + priority, 0)
 
+const golf1 = () => i.split`\n`.map(l=>l.split``.find(c=>l.lastIndexOf(c)>=l.length/2).charCodeAt(0)).reduce((t,c)=>t+(c>90?c-96:c-38),0)
+
 const part2 = () =>
-  new Array(lines.length / 3)
-    .fill('')
+  new Array(lines.length / 3).fill('')
     .map(_ => lines.splice(0, 3))
     .map(([first, second, third]) => first.split('').find(char => second.includes(char) && third.includes(char)))
     .map(getPriority)
     .reduce((sum, curr) => sum + curr, 0)
 
+
 console.log('Part 1:', part1())
+console.log('Golf 1', golf1())
 console.log('Part 2:', part2())
