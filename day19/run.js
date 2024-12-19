3e86e8dfaf14a2cd59cc891c21b32a8a52942ff2
Ian Germann <ianjgermann@gmail.com>
2024-12-19 02:12:17 -0500

    day19

--- day19/run.js
+++ day19/run.js
@@ -7,43 +7,34 @@ const inputFile = args[0] ?? "input.txt";
 const input = fs.readFileSync(inputFile, "utf8").toString();
 const [towelInput, designInput] = input.split("\n\n");
 
-const towels = towelInput.split(", ").filter((x) => x !== "")
-const designs = designInput.split("\n").filter((x) => x !== "")
+const towels = towelInput.split(", ")
+const designs = designInput.split("\n")
+
+let cache = new Map();
+const getPossibleDesigns = (design) => {
+  if (design === "") return 1;
+  if (cache.has(design)) return cache.get(design)
+
+  const result = towels.reduce((total, towel) =>
+    design.startsWith(towel)
+      ? total + getPossibleDesigns(design.substring(towel.length))
+      : total
+  , 0)
+  cache.set(design, result)
+  return result;
+};
 
 const part1 = () => {
-  const cache = new Map()
-  const isPossible = (design, towels) => {
-    if (design === "") return true
-    if (cache.has(design)) return cache.get(design)
-
-    const possible = towels.some(towel => {
-      if (design.startsWith(towel)) return isPossible(design.replace(towel, ""), towels)
-      return false;
-    })
-    cache.set(design, possible)
-    return possible
-  }
-
-  return designs.filter(design => isPossible(design, towels)).length
+  return designs
+    .map(getPossibleDesigns)
+    .filter(n => n > 0)
+    .length
 }
 
 const part2 = () => {
-  let cache = new Map();
-
-  const getPossibleDesigns = (design, towels) => {
-    if (design === "") return 1;
-    if (cache.has(design)) return cache.get(design)
-
-    const toTest = towels.filter((towel) => design.startsWith(towel));
-    const result = toTest.reduce((total, towel) => {
-      return total + getPossibleDesigns(design.replace(towel, ""), towels)
-    }, 0)
-    cache.set(design, result)
-    return result;
-  };
-
-  const possibleDesigns = designs.map((design) => getPossibleDesigns(design, towels));
-  return possibleDesigns.reduce((total, designs) => total + designs, 0);
+  return designs
+    .map(getPossibleDesigns)
+    .reduce((total, n) => total + n, 0);
 }
 
 time("part 1");
