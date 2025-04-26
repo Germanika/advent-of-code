c1247346cf852531b8ee7215e5b5121349cf435b
Ian Germann <ianjgermann@gmail.com>
2025-04-26 16:06:16 -0400

    add timing to template

--- 2023/day7/index.js
+++ 2023/day7/index.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { log, time, timeEnd } from "console";
 
 const args = process.argv.slice(2);
 const inputFile = args[0] ?? "input.txt";
@@ -166,5 +167,10 @@ const part2 = () => {
     }, 0);
 };
 
-console.log("Part 1:", part1());
-console.log("Part 2:", part2());
+time("Part 1");
+log("Part 1:", part1());
+timeEnd("Part 1");
+
+time("Part 2");
+log("Part 2:", part2());
+timeEnd("Part 2");
--- 2023/template/index.js
+++ 2023/template/index.js
@@ -1,18 +1,20 @@
-import fs from 'fs'
+import fs from "fs";
+import { log, time, timeEnd } from "console";
 
-const args = process.argv.slice(2)
-const inputFile = args[0] ?? 'input.txt'
+const args = process.argv.slice(2);
+const inputFile = args[0] ?? "input.txt";
 
-const input = fs.readFileSync(inputFile, 'utf8').toString();
-const lines = input.split('\n')
+const input = fs.readFileSync(inputFile, "utf8").toString();
+const lines = input.split("\n");
 
-const part1 = () => {
+const part1 = () => {};
 
-}
+const part2 = () => {};
 
-const part2 = () => {
+time("Part 1");
+log("Part 1:", part1());
+timeEnd("Part 1");
 
-}
-
-console.log('Part 1:', part1())
-console.log('Part 2:', part2())
+time("Part 2");
+log("Part 2:", part2());
+timeEnd("Part 2");
